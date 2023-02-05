e14580eef3f9abf56c48c9248ecba2064edb2b1c
Matthieudegny <matthieu.dev.degny@gmail.com>
2023-02-05 16:44:16 +0100

    test without routes

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const connectDB = require("./config/connectDB");
 const cors = require("cors");
 
 const app = express();
-const dashboardRoutes = require("./api/routes/dashboard");
-const userRoutes = require("./api/routes/user");
+// const dashboardRoutes = require("./api/routes/dashboard");
+// const userRoutes = require("./api/routes/user");
 
 //middleware
 //pour accéder aux bodys des requests
@@ -15,8 +15,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //routes
-app.use("/api/user", userRoutes);
-app.use("/api/dashboard", dashboardRoutes);
+// app.use("/api/user", userRoutes);
+// app.use("/api/dashboard", dashboardRoutes);
 
 const PORT = process.env.PORT || 5000;
 
