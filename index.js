8b94b8b51da14d25959fd08de476c1ef1b9d9831
Matthieudegny <matthieu.dev.degny@gmail.com>
2023-02-05 17:07:26 +0100

    removed mongo config

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const connectDB = require("./config/connectDB");
+// const connectDB = require("./config/connectDB");
 // const cors = require("cors");
 
 const app = express();
